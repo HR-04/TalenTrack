b7af911c07daec00101aa70c2df5ea4d727b1157
Kavya <115682752+KavyaPalanichamy@users.noreply.github.com>
2024-10-28 09:25:24 +0530

    Update home.py

--- home.py
+++ home.py
@@ -16,7 +16,7 @@ def app():
     # Add an image or logo to make it visually appealing
     import os
 
-    st.image("img/career.jpg", use_column_width=True)
+    st.image("img/career.png", use_column_width=True)
 
 
 
